c29e55e64c654ad5e03cfbf35017a04d2b606525
EDE MANIDEEP <manideepede9@gmail.com>
2024-08-14 21:32:04 +0530

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -3,6 +3,14 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
+            title : 'GitHub Foundations',
+            details : 'The GitHub Foundations Certification validates foundational knowledge of version control, repository management, and collaboration using GitHub.',
+            date : '2024',
+            field : 'Version Control',
+            url:"https://www.credly.com/badges/6c4506ef-d341-46f5-879d-20354776cfc5/",
+            image : 'https://img.icons8.com/?size=100&id=12599&format=png&color=000000'
+        },{
+            id : 2,
             title : 'AZ - 900 certification',
             details : 'The AZ-900 exam is a Microsoft certification that validates foundational knowledge of cloud concepts and Microsoft Azure.',
             date : '2023',
@@ -11,7 +19,7 @@ export const achievementData = {
             image : 'https://img.icons8.com/?size=100&id=VLKafOkk3sBX&format=png&color=000000'
         },
         {
-            id : 2,
+            id : 3,
             title : 'Oracle Cloud Infrastructure',
             details : 'Oracle Cloud Infrastructure is a cloud computing platform that provides a wide range of services, including compute, storage, networking, and databases.',
             date : '2023',
@@ -20,7 +28,7 @@ export const achievementData = {
             image : 'https://img.icons8.com/?size=100&id=39913&format=png&color=000000'
         },
         {
-            id : 3,
+            id : 4,
             title : 'Automation Anywhere Certified Essentials RPA Professional',
             details : 'validates your knowledge of RPA fundamentals and your ability to use the Automation Anywhere platform to automate business processes.',
             date : '2023',
@@ -29,7 +37,7 @@ export const achievementData = {
             image : 'https://logowik.com/content/uploads/images/automation-anywhere1292.logowik.com.webp'
         },
         {
-            id :3 ,
+            id :5,
             title : 'HackerRank Certifications',
             details : 'HackerRank certifications validate your coding skills and problem-solving abilities in various programming languages and technical domains.',
             date : '2024',
