bae8a5ebb0a6a5d34b5bcfd549a1367a872c4ae5
EDE MANIDEEP <manideepede9@gmail.com>
2024-09-20 22:31:06 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,34 +1,55 @@
-import one from '../assets/svg/projects/temperature_converter_icon.png'
-import two from '../assets/svg/projects/rock-paper-scissors.png'
-import three from '../assets/svg/projects/live-clock.png'
+import three from '../assets/svg/projects/temperature_converter_icon.png'
+import four from '../assets/svg/projects/rock-paper-scissors.png'
+import five from '../assets/svg/projects/live-clock.png'
 
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Restaurant Web App - ( Flavors of India )',
+        projectDesc: 'Developed a web application for an Indian restaurant, enabling users to browse the menu, make reservations, and place
+pickup orders. Built with a responsive React.js frontend and a Node.js backend, the app efficiently handles user data, order
+processing, and customer information storage',
+        tags: ['Visual Studio Code', 'CockroachDB', 'SheetDB', 'React.js', 'Node.js', 'JSX', 'CSS'],
+        code: 'https://github.com/EdeManideep/react-restaurant-app.git',
+        demo: 'https://react-restaurant-app1.netlify.app/',
+        image: three
+    },
+    {
+        id: 2,
+        projectName: 'Online Voting System',
+        projectDesc: 'Developed an Online Voting System emphasizing robust backend functionality and secure data management. This system
+ensures a reliable voting experience, allowing voters to conveniently cast their votes from remote locations',
+        tags: ['Spring Tool Suite', 'MySQL', 'Java', 'HTML', 'CSS', 'JavaScript'],
+        code: '#',
+        demo: '#',
+        image: three
+    },
+    {
+        id: 3,
         projectName: 'Temperature Converter',
         projectDesc: 'A responsive temperature converter website with input validation and conversion options for Celsius, Fahrenheit, and Kelvin.',
         tags: ['HTML', 'CSS', 'JavaScript'],
         code: 'https://github.com/EdeManideep/OIBSIP/tree/main/Temperature%20Converter',
         demo: 'https://temperatureconverter-1.netlify.app/',
-        image: one
+        image: three
     },
     {
-        id: 2,
+        id: 4,
         projectName: 'Rock Paper Scissors Game',
         projectDesc: 'Interactive Rock Paper Scissors game with score tracking and history, developed using HTML, CSS, and JavaScript, deployed on Netlify',
         tags: ['HTML', 'CSS', 'JavaScript'],
         code: 'https://github.com/EdeManideep/rock-paper-scissors/tree/main/rock-paper-scissors',
         demo: 'https://rock-paper-scissors-online-game.netlify.app/',
-        image: two
+        image: four
     },
     {
-        id: 3,
+        id: 5,
         projectName: 'Live Clock',
         projectDesc: 'A real-time clock application with a dark and light mode toggle, displaying current time and date across multiple time zones',
         tags: ['HTML', 'CSS', 'JavaScript'],
         code: 'https://github.com/EdeManideep/Live_Clock',
         demo: 'https://liveclock1.netlify.app/',
-        image: three
+        image: five
     },
     
 ]
