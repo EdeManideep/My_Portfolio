c9df26b240239723764bbb9f593f6f8d7a1f2c73
EDE MANIDEEP <manideepede9@gmail.com>
2024-09-25 23:22:52 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,13 +3,16 @@ import two from '../assets/svg/projects/vote.png';
 import three from '../assets/svg/projects/temperature_converter_icon.png';
 import four from '../assets/svg/projects/rock-paper-scissors.png';
 import five from '../assets/svg/projects/live-clock.png';
+import six from '../assets/svg/projects/Captcha-Generator-Project.png';
+import seven from '../assets/svg/projects/grocery-items-list-web-site.png';
+import eight from '../assets/svg/projects/YouTube-Demo-Project.png';
 
 export const projectsData = [
   {
     id: 1,
     projectName: 'Restaurant Web App',
-    projectDesc: 'Developed a web application for an Indian restaurant, enabling users to browse the menu, make reservations, and place pickup orders. Built with a responsive React.js frontend and a Node.js backend, the app efficiently handles user data, order processing, and customer information storage.',
-    tags: ['Visual Studio Code', 'CockroachDB', 'SheetDB', 'React.js', 'Node.js', 'JSX', 'CSS'],
+    projectDesc: 'Developed a web application for an Indian restaurant with a React.js frontend and Node.js backend, enabling users to browse the menu, make reservations, and place pickup orders.',
+    tags: ['Visual Studio Code', 'CockroachDB', 'SheetDB', 'React.js, Node.js', 'JSX, CSS'],
     code: 'https://github.com/EdeManideep/react-restaurant-app.git',
     demo: 'https://react-restaurant-app1.netlify.app/',
     image: one
@@ -18,9 +21,9 @@ export const projectsData = [
     id: 2,
     projectName: 'Online Voting System',
     projectDesc: 'Developed an Online Voting System emphasizing robust backend functionality and secure data management. This system ensures a reliable voting experience, allowing voters to conveniently cast their votes from remote locations.',
-    tags: ['Spring Tool Suite', 'MySQL', 'Java', 'HTML', 'CSS', 'JavaScript'],
-    code: '',
-    demo: '',
+    tags: ['Spring Tool Suite', 'MySQL, Java', 'HTML, CSS', 'JavaScript'],
+    code: 'https://github.com/EdeManideep/Online_Voting_Web_App.git',
+    demo: 'https://github.com/EdeManideep/Online_Voting_Web_App.git',
     image: two
   },
   {
@@ -49,6 +52,32 @@ export const projectsData = [
     code: 'https://github.com/EdeManideep/Live_Clock',
     demo: 'https://liveclock1.netlify.app/',
     image: five
+  },
+  {
+    id: 6,
+    projectName: 'Captcha-Generator',
+    projectDesc: 'Developed a Captcha Generator using HTML, CSS, and JavaScript, featuring a refresh button, submit button, input box, and alert messages for user validation.',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    code: 'https://github.com/EdeManideep/Captcha-Generator.git',
+    demo: 'https://edemanideep.github.io/Captcha-Generator/Captcha.html',
+    image: six
+  },
+  {
+    id: 7,
+    projectName: 'Grocery Items List',
+    projectDesc: 'I developed a Grocery Items List Web application using HTML, CSS, JavaScript, and React displaying grocery items names.',
+    tags: ['HTML', 'CSS', 'JavaScript', 'React'],
+    code: 'https://github.com/EdeManideep/grocery-items-list.git',
+    demo: 'https://grocery-items-list-web-site.netlify.app/',
+    image: seven
+  },{
+    id: 8,
+    projectName: 'Youtube Demo',
+    projectDesc: 'I developed a YouTube demo using HTML and CSS for learning purposes.',
+    tags: ['HTML', 'CSS'],
+    code: 'https://github.com/EdeManideep/Youtube_demo.git',
+    demo: 'https://edemanideep.github.io/Youtube_demo/youtube.html',
+    image: eight
   }
 ];
 
