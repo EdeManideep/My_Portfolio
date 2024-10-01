4b2bfcf68db6cc0974a4e64681ea3756327a6981
EDE MANIDEEP <manideepede9@gmail.com>
2024-10-01 23:45:31 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from '../assets/pdf/2100039095_EDE_MANIDEEP_KLU_2025_RESUME_NO_PIC_SOFTWARE_HEADER.pdf'
+import resume from '../assets/pdf/2100039095_EDE_MANIDEEP_KLU_2025_RESUME.pdf'
 import profile_image from '../assets/PASSPORT_SIZE_PHOTO-White - Copy.png'
 
 export const headerData = {
