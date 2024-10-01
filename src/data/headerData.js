0592310045fba55ee3b7bdf2b2a2cce7a3e932a9
EDE MANIDEEP <manideepede9@gmail.com>
2024-10-01 23:48:04 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import profile_image from '../assets/PASSPORT_SIZE_PHOTO-White - Copy.png'
 export const headerData = {
     name: 'Ede Manideep',
     title: "",
-    desciption:"I am a data science enthusiast with strong Python programming skills and experience in web development. I am excited to join the software industry and apply my knowledge and skills to real-world challenges.",
+    desciption:"As a computer science undergraduate with strong Programming skills and a passion for web development, I am eager to start my career in the software industry. I have experience developing dynamic web applications and have recently learned React.js, which has equipped me to build modern, interactive user interfaces and enhance the user experience in software projects.",
     image: profile_image,
     resumePdf: resume
 }
